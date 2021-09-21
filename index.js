5501dbb274ca738c3b0fdc67808c127c3286baab
wesley_wisch <wesley_wisch@hotmail.com>
2021-09-21 07:49:52 -0300

    Html pronto e começando o javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+const dummyTransactions = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Slário', amount: 300 },
+    { id: 3, name: 'Torta de frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 },
+];
+
+function addTransactionIntoDOM(transaction) {
+    
+
+    <li class="minus">
+        Salário <span>-$400</span><button class="delete-btn">X</button>
+    </li>
+}
