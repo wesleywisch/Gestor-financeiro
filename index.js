aaf79d3218771cdc9b2ded7c3bf165d4ea5399c2
wesley_wisch <wesley_wisch@hotmail.com>
2021-09-22 07:35:49 -0300

    Criando a li via javascript e ja adicionando no html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const transactionsUl = document.querySelector('#transactions');
+
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Slário', amount: 300 },
@@ -6,9 +8,17 @@ const dummyTransactions = [
 ];
 
 function addTransactionIntoDOM(transaction) {
-    
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const li = document.createElement('li');
 
+    li.classList.add(CSSClass);
+    li.innerHTML = `
     <li class="minus">
-        Salário <span>-$400</span><button class="delete-btn">X</button>
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">X</button>
     </li>
+    `;
+
+    transactionsUl.prepend(li);
 }
